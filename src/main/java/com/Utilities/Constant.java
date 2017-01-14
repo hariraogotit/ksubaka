f5f78b556cc642c2dfcc3a0ce43e1b6a921d1cd3
Hari Rao <hari.rao@outlook.com>
2017-01-14 11:28:03 +0000

    xml file that can be opened in https://www.draw.io/

---
+++ src/main/java/com/Utilities/Constant.java
@@ -0,0 +1,10 @@
+package com.Utilities;
+
+/**
+ * Created by Hari Rao on 12/01/17.
+ */
+public class Constant {
+
+    public static final String MOVIE_TYPE = "movie";
+    public static final String MUSIC_TYPE = "music";
+}
